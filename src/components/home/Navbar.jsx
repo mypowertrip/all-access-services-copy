27088c0422cda68e85e5c1665cb9777f738fc9cb
Tim Scheer <tim@mypowertrip.com>
2026-04-22 07:16:00 +0000

    Visual edits

--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -54,7 +54,7 @@ export default function Navbar() {
 
           {/* Logo */}
           <a href="/" className="flex items-center flex-shrink-0">
-            <img src="https://media.base44.com/images/public/69e03c311db29c3c17ba7e75/3acd3aa38_ChatGPT_Image_Apr_22__2026__12_09_54_AM.png"
+            <img src="https://media.base44.com/images/public/69e03c311db29c3c17ba7e75/c480996d7_ChatGPT_Image_Apr_22__2026__12_15_08_AM.png"
 
             alt="All Access Services" className="h-36 w-auto object-contain"
 
